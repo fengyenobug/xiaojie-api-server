5ee433f3e51823d33bcd2fea36a531ad814cc4f8
jiege <1768669274@qq.com>
2024-04-07 21:29:45 +0800

    引入bodyparser插件，解析json数据

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import logger from 'morgan'
+import bodyParser from 'body-parser'
 import { expressjwt } from 'express-jwt'
 const app = express()
 // 导入 dotenv 以注入环境变量
@@ -11,29 +12,29 @@ app.use(logger('dev'));
 // 注意：只要配置成功了 express-jwt 这个中间件，就可以把解析出来的用户信息，挂载到 req.user 属性上
 app.use(expressjwt({ secret: process.env.JWT_SECRET_KEY, algorithms: ['HS256'] }).unless({ path: ["/login", /^\/api\//, "/reguser"] }))
 app.use(express.urlencoded({ extended: false }))
-
+app.use(bodyParser.json())
 app.use((req, res, next) => {
-    res.cc = (err, status = 500) => {
-        res.send({
-            status,
-            message: err instanceof Error ? err.message : err
-        })
-    }
-    next()
+  res.cc = (err, status = 500) => {
+    res.send({
+      status,
+      message: err instanceof Error ? err.message : err
+    })
+  }
+  next()
 })
 
 app.use((err, req, res, next) => {
-    // 这次错误是由 token 解析失败导致的
-    if (err.name === 'UnauthorizedError') {
-        return res.send({
-            status: 401,
-            message: '无效的token',
-        })
-    }
-    res.send({
-        status: 500,
-        message: '未知的错误',
+  // 这次错误是由 token 解析失败导致的
+  if (err.name === 'UnauthorizedError') {
+    return res.send({
+      status: 401,
+      message: '无效的token',
     })
+  }
+  res.send({
+    status: 500,
+    message: '未知的错误',
+  })
 })
 
 import apiRouter from './router/apiRouter.js'
@@ -44,5 +45,5 @@ app.use(whiteListRouter)
 app.use(userRouter)
 
 app.listen(3000, () => {
-    console.log('express server running at 127.0.0.1:3000');
+  console.log('express server running at 127.0.0.1:3000');
 })
--- router_handler/whiteList.js
+++ router_handler/whiteList.js
@@ -5,60 +5,60 @@ import * as schemaUser from '../schema/regSchema.js'
 import { encrypto } from '../utils/aes.js'
 
 export const regUser = async (req, res) => {
-    try {
-        const userinfo = req.body
-        const { err, value } = schemaUser.regSchema.validate(userinfo)
-        if (err) {
-            return res.cc(err)
-        }
-        // 查询数据库，判断当前用户名是否已被占用
-        const sqlStr = 'select * from users where username = ?'
-        db.query(sqlStr, userinfo.username, (err, result) => {
-            if (err) {
-                return res.cc(err)
-            }
-            if (result.length > 0) {
-                return res.cc('用户名被占用，请更换其他用户名！', 402)
-            }
-            // 防止用户密码泄露，对存入数据库的密码进行加密，避免明文储存密码
-            const password = bcrypt.hashSync(userinfo.password, 10)
-            // 对邮箱加密存储
-            const email = encrypto(userinfo.email)
-            const sql = 'insert into users set ?'
-            db.query(sql, { username: userinfo.username, password: password, email: email }, (err, result) => {
-                if (err) return res.cc(err)
-                if (result.affectedRows !== 1) return res.cc('注册失败，请稍后再试！')
-                return res.cc('注册成功', 200)
-            })
-        })
-    } catch (error) {
-        return res.cc(error)
+  try {
+    const userinfo = req.body
+    const { err, value } = schemaUser.regSchema.validate(userinfo)
+    if (err) {
+      return res.cc(err)
     }
+    // 查询数据库，判断当前用户名是否已被占用
+    const sqlStr = 'select * from users where username = ?'
+    db.query(sqlStr, userinfo.username, (err, result) => {
+      if (err) {
+        return res.cc(err)
+      }
+      if (result.length > 0) {
+        return res.cc('用户名被占用，请更换其他用户名！', 402)
+      }
+      // 防止用户密码泄露，对存入数据库的密码进行加密，避免明文储存密码
+      const password = bcrypt.hashSync(userinfo.password, 10)
+      // 对邮箱加密存储
+      const email = encrypto(userinfo.email)
+      const sql = 'insert into users set ?'
+      db.query(sql, { username: userinfo.username, password: password, email: email }, (err, result) => {
+        if (err) return res.cc(err)
+        if (result.affectedRows !== 1) return res.cc('注册失败，请稍后再试！')
+        return res.cc('注册成功', 200)
+      })
+    })
+  } catch (error) {
+    return res.cc(error)
+  }
 
 }
 export const login = (req, res) => {
-    try {
-        const userInfo = req.body
-        const sql = 'select * from users where username = ?'
-        db.query(sql, userInfo.username, (err, result) => {
-            if (err) return res.cc(err)
-            if (result.length === 1) {
-                const succ = bcrypt.compareSync(userInfo.password, result[0].password)
-                if (succ) {
-                    const tokenStr = Jwt.sign({ username: userInfo.username }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' })
-                    return res.send({
-                        status: 200,
-                        message: '登陆成功！',
-                        token: tokenStr
-                    })
-                } else {
-                    return res.cc('密码错误', 402);
-                }
-            } else {
-                res.cc('用户名不存在！', 402)
-            }
-        })
-    } catch (error) {
-        return res.cc(error.message)
-    }
+  try {
+    const userInfo = req.body
+    const sql = 'select * from users where username = ?'
+    db.query(sql, userInfo.username, (err, result) => {
+      if (err) return res.cc(err)
+      if (result.length === 1) {
+        const succ = bcrypt.compareSync(userInfo.password, result[0].password)
+        if (succ) {
+          const tokenStr = Jwt.sign({ username: userInfo.username }, process.env.JWT_SECRET_KEY, { expiresIn: '7d' })
+          return res.send({
+            status: 200,
+            message: '登陆成功！',
+            token: tokenStr
+          })
+        } else {
+          return res.cc('密码错误', 402);
+        }
+      } else {
+        res.cc('用户名不存在！', 402)
+      }
+    })
+  } catch (error) {
+    return res.cc(error.message)
+  }
 }
