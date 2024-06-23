d1fb2f5f5ffca51713589754dc00c750136a5640
jiege <1768669274@qq.com>
2024-06-23 21:59:08 +0800

    数据库host设置动态

--- db/index.js
+++ db/index.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql'
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: '3306',
-    user: process.env.DB_USER,
-    password: process.env.DB_PWD,
-    database: 'xiaojieapi'
+  host: process.env.DB_HOST,
+  port: '3306',
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: 'xiaojieapi'
 })
 
 export default db
